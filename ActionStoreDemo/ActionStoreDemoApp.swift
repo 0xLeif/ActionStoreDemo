3ba4dd18347030fe6e6cec95688b109cefe423cd
0xLeif <zmeriksen@icloud.com>
2022-05-17 18:05:09 -0600

    Initial Commit

---
+++ ActionStoreDemo/ActionStoreDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionStoreDemoApp.swift
+//  ActionStoreDemo
+//
+//  Created by 0x on 5/17/22.
+//
+
+import SwiftUI
+
+@main
+struct ActionStoreDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
